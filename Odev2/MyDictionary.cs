c71b3d7b6f6871853c2cc087fb74bce3fbb6d28e
feyza yılmaz <feyzayilmaz1998@hotmail.com>
2021-11-25 22:02:25 +0300

    Odev2

---
+++ Odev2/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev2
+{
+    class MyDictionary<K,T>
+    {
+        K[] diziAnahtar;
+        T[] diziDeger;
+        public MyDictionary()
+        {
+            diziAnahtar = new K[0];
+            diziDeger = new T[0];
+
+        }
+
+        public void Add(K anahtar, T deger)
+        {
+            K[] tempAnahtar = diziAnahtar;// ANAHTAR kaybı olmasın diye geçiçi anahtarları tutacak dizi
+            T[] tempDeger = diziDeger;// DEGER kaybı olmasın diye geçiçi anahtarları tutacak dizi
+
+            diziAnahtar = new K[tempAnahtar.Length + 1]; //asıl olan dizimi ve yeniden hafızaya +1 ekleyerek yer açıyoruz.
+            diziDeger = new T[tempDeger.Length + 1];//asıl olan dizimi ve yeniden hafızaya +1 ekleyerek yer açıyoruz.
+
+
+            for (int i = 0; i < tempAnahtar.Length; i++) //// döngü ile geçiçi devrettiğimiz değerlerimizi tekrar alıyoruz.
+                {
+                diziAnahtar[i] = tempAnahtar[i];
+                diziDeger[i] = tempDeger[i];
+
+            }
+            diziAnahtar[diziAnahtar.Length - 1] = anahtar; // yeni anahtarları yazıyoruz.
+            diziDeger[diziDeger.Length - 1] = deger; // yeni degeri yazıyoruz.
+
+        }
+       
+        public int Length  // dizimizde toplam kaç boyutlu sayıyoruz.
+        {
+            get { return diziAnahtar.Length; }
+        }
+
+    }
+}
---
+++ Odev2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Odev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(7,"feyza");
+            myDictionary.Add(10, "yılmaz");
+            myDictionary.Add(12, "asil");
+            Console.WriteLine(myDictionary.Length);
+
+        }
+    }
+}

4b7fff2b3bf6564cd7fa302dbc2d1a92a119d1b1
feyza yılmaz <feyzayilmaz1998@hotmail.com>
2021-11-26 23:50:35 +0300

    26.11

---
+++ InterfaceAbstractDemo/Abstract/BaseCustomerManager.cs
@@ -0,0 +1,15 @@
+﻿using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Abstract
+{
+    public abstract class  BaseCustomerManager : ICustomerService
+    {
+        public void Save(Customer customer)
+        {
+            Console.WriteLine("Veritabanına kaydedildi.");
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using InterfaceAbstractDemo.Abstract;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+
+    }
+}
